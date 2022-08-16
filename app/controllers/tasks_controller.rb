c1e1f5277fd7c6e41a212aa95584dc0b3631501e
sofia larrea <sofialejandra.larrea@gmail.com>
2022-08-16 16:48:09 -0300

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+ before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  
+  end
+
+  def update
+
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def sat_task
+    @task = Task.find(params[:id])
+  end
+end
