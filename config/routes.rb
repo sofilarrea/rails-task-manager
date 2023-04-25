c1a755eda47cb61f0faa950d34ad3bf532b1b4a8
sofilarrea <sofialejandra.larrea@gmail.com>
2023-04-25 14:17:20 -0300

    starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  #creat
- 
+  # Create
+  get 'taks', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 end
