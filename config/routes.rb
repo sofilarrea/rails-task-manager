7a84d449a28b50452be68b7bdc4ecc03be7c027c
sofilarrea <sofialejandra.larrea@gmail.com>
2023-04-25 15:29:12 -0300

    form_with

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
   # Read
-  get 'tasks', to: 'tasks/index'
-  get 'tasks/:id', to: 'tasks/show', as: :task
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
