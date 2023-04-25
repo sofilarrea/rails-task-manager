8d10e0e3306731c9a4245145c182674697f7161c
sofilarrea <sofialejandra.larrea@gmail.com>
2023-04-25 15:38:17 -0300

    Create accion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
