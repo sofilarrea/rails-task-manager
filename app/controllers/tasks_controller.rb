3d33af3de1746959620519509b5ec6f19b46f89f
sofilarrea <sofialejandra.larrea@gmail.com>
2023-04-26 10:03:45 -0300

    Ready

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,43 +1,43 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update delete]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new(task_params)
-    if @task.save
-      redirect_to task_path
-    else
-      render :new, status: :unproccesable_entity
-    end
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
   end
 
-  def index
-    @task = Task.all
+  def edit
   end
-
-  def show; end
-
-  def edit; end
 
   def update
     @task.update(task_params)
-    redirect_to task_path
+    redirect_to task_path(@task)
   end
 
   def destroy
     @task.destroy
-    redirect_to task_path
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
-  end
-
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
